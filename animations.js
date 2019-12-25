52bdb78cd10449c114481e8aabf06953eb78a49d
Ricardo Tlatelpa <rtlatlepa4@gmail.com>
2019-12-25 14:35:10 -0500

    mobile navigation

--- animations.js
+++ animations.js
@@ -1,1 +1,10 @@
-console.log('im gonna add some animation')
+const navSlide = () =>{
+    const burger = document.querySelector('.burger');
+    const header = document.querySelector('.header-menu');
+   
+    burger.addEventListener('click', () =>{
+        header.classList.toggle('header-active');
+    });
+}
+
+navSlide();
