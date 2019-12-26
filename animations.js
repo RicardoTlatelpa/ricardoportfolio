0113e6da1df0d64cc50fd4afb634e2042d67dd4d
Ricardo Tlatelpa <rtlatlepa4@gmail.com>
2019-12-25 23:08:13 -0500

    link animation

--- animations.js
+++ animations.js
@@ -1,10 +1,19 @@
 const navSlide = () =>{
     const burger = document.querySelector('.burger');
     const header = document.querySelector('.header-menu');
-   
+    const navLinks = document.querySelectorAll('.header-menu a');
     burger.addEventListener('click', () =>{
         header.classList.toggle('header-active');
+        navLinks.forEach((link,index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.4}s`;
+            }
+          
+        })
     });
+   
 }
 
 navSlide();
